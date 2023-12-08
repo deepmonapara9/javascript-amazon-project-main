b8eefaa79ed542d7afed2ed6a973be4a52e6d837
deepmonapara9 <deepmonapara0@gmail.com>
2023-12-09 00:55:14 +0530

    delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -98,3 +98,13 @@ cart.forEach((cartItem) => {
 // console.log(cartSummaryHTML);
 
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+  link.addEventListener('click', () => {
+    // console.log('delete');
+    const productId = link.dataset.productId;
+    // console.log(productId);
+    removeFromCart(productId);
+    // console.log(cart);
+  });
+});
